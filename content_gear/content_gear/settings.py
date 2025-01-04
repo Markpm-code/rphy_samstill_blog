739317d6ddf389612e9041175f98ac6968cfe9ea
Marlon <you@example.com>
2025-01-04 11:05:55 +0000

    add apps

--- content_gear/content_gear/settings.py
+++ content_gear/content_gear/settings.py
@@ -41,6 +41,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'blogging_tools',
+    'photography',
+    'videography',
 ]
 
 MIDDLEWARE = [
